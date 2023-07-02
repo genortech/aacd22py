812fb6ad5c1aacff7badc49fa1a9dde897a8de36
lumprais <genortech@gmail.com>
2023-07-02 23:43:40 +1000

    Day two updated

--- day002/main.py
+++ day002/main.py
@@ -1,14 +1,20 @@
 player_one = []
 player_two = []
 
-with open("input.txt") as data:
-    # print (data.read(1))
-    while True:
-        if data.read(1) == 'A' or 'B' or 'C':
-            player_one = data.read(1)
-        elif data.read(1) == 'X' or 'Y' or 'Z':
-            playeer_two = data.read(1)
-        elif not data.read(1):
+with open("input.txt", 'r') as data:
+
+    while 1:
+
+        char = data.read(1)
+        if not char:
             break
-    print ("player one", player_one)
-    print ("player two", player_two)
+        print ("data 2" , char)
+
+        if char in ('A','B','C'):
+            player_one.append(char)
+        elif char in ('X','Y','Z'):
+            player_two.append(char)
+
+
+print ("player one", player_one)
+print ("player two", player_two)
