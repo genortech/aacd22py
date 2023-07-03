725228c2b2b5ed3f7fa3f1759c464c9487c3f43c
lumprais <genortech@gmail.com>
2023-07-03 21:35:19 +1000

    Day two updated

--- day002/main.py
+++ day002/main.py
@@ -1,5 +1,19 @@
 player_one = []
 player_two = []
+player_one_score  = 0
+player_two_score = 0
+
+#A rock
+#B paper
+#C scissors
+
+#Y rock 1pt
+#X paper 2pt
+#Z scissors 3pl
+
+#win 6pt
+#draw 3pt
+#loose 0pt
 
 with open("input.txt", 'r') as data:
 
@@ -8,7 +22,6 @@ with open("input.txt", 'r') as data:
         char = data.read(1)
         if not char:
             break
-        print ("data 2" , char)
 
         if char in ('A','B','C'):
             player_one.append(char)
@@ -16,5 +29,29 @@ with open("input.txt", 'r') as data:
             player_two.append(char)
 
 
-print ("player one", player_one)
-print ("player two", player_two)
+# print ("player one", player_one)
+# print ("player two", player_two)
+
+    for item , item1 in zip(player_one, player_two):
+        # print(f'Item 1: {item}', f'Item 2: {item1}')
+
+        if item == 'A' and item1 =='X':
+            player_two_score += 1 + 3
+        if item == 'A' and item1 =='Y':
+            player_two_score += 2 + 6
+        if item == 'A' and item1 =='Z':
+            player_two_score += 3
+        if item == 'B' and item1 =='X':
+            player_two_score += 1
+        if item == 'B' and item1 =='Y':
+            player_two_score += 2 + 3
+        if item == 'B' and item1 =='Z':
+            player_two_score += 3 + 6
+        if item == 'C' and item1 =='X':
+            player_two_score += 1 + 6
+        if item == 'C' and item1 =='Y':
+            player_two_score += 2
+        if item == 'C' and item1 =='Z':
+            player_two_score += 3 + 3
+
+    print("player two score", player_two_score)
