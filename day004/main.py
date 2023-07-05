e8c0cb7be040ca64b5f7c3f15e4b18bc91f73861
lumprais <genortech@gmail.com>
2023-07-05 23:04:58 +1000

    Day four updated

--- day004/main.py
+++ day004/main.py
@@ -4,17 +4,21 @@ with open("input.txt", 'r') as data:
 
 pairs = 0
 for section in pairs:
+    print(section)
     section1, section2 = section.split(',')
-    print(section1, section2)
+    # print(section1, section2)
 
     section1 = [int(i) for i in section1.split('-')]
     section2 = [int(i) for i in section2.split('-')]
 
-    print(section1, section2)
+    print('sect1', section1,'sect2', section2)
 
+    if section1[0] <= section2[0] and section1[1] >= section2[1]:
+        count +=1
+    elif section1[0] >= section2[0] and section1[1] <= section2[1]:
+        count+=1
 
-
-
+    print (count)
 
 
 
